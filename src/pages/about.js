37c22e31c6527a41e0fd7668682e5681356fa732
michaeljamie <familiarfletcher@gmail.com>
2019-06-17 21:55:53 -0600

    original commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+    <>
+        <h1>About Me</h1>
+        <p>This is my personal blog.</p>
+        <Link to="/">&larr; Back to Home</Link>
+    </>
+)
