89303f17386f514e03433dc2252dfe966e611c3c
michaeljamie <familiarfletcher@gmail.com>
2019-06-17 22:46:58 -0600

    added global styling and nav

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
+
 
 export default () => (
-    <>
+    <Layout>
         <h1>About Me</h1>
         <p>This is my personal blog.</p>
         <Link to="/">&larr; Back to Home</Link>
-    </>
+    </Layout>
 )
